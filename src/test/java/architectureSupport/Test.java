2aa6d71d8ef4cd6572c5e805d58ba41bd1ed1eb0
=Apurva <=apurvanaik2023@gmail.com>
2023-08-14 14:58:42 +0530

    Develop branch comit --Architectural changes

---
+++ src/test/java/architectureSupport/Test.java
@@ -0,0 +1,12 @@
+package architectureSupport;
+
+public class Test {
+
+    public void pomModel(){
+        System.out.println("Page Object Model");
+    }
+
+    public void extentReports(){
+        System.out.println("extentReports");
+    }
+}
